d407003b4fa57efd77206fefc30627376c992a4f
mostar <mominahmad93@gmail.com>
2018-08-09 14:16:22 -0500

    cleaned up user info

--- index.js
+++ index.js
@@ -10,24 +10,78 @@ var config = {
 
   var database = firebase.database();
 
+  var option = false;
 
+  $("#option-rider").on('click', function(){
+    if (option === true) {
+      option === false;
+    } else {
+      return false;
+    }
+  })
+  $("#option-driver").on('click', function(){
+    if (option === false) {
+      option === true;
+    } else {
+      return true;
+    }
+  })
 
+  // (function() {
+  //   'use strict';
+  //   window.addEventListener('load', function() {
+  //     // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  //     var forms = document.getElementsByClassName('needs-validation');
+  //     // Loop over them and prevent submission
+  //     var validation = Array.prototype.filter.call(forms, function(form) {
+  //       form.addEventListener('submit', function(event) {
+  //         if (form.checkValidity() === false) {
+  //           event.preventDefault();
+  //           event.stopPropagation();
+  //         }
+  //         form.classList.add('was-validated');
+  //       }, false);
+  //     });
+  //   }, false);
+  // })();
+  
   $("#sign-up").on('click', function(event){
-      event.preventDefault();
-      username = $("#exampleInputEmail1").val().trim();
-      password = $("#exampleInputPassword1").val().trim();
+    event.preventDefault();
+    user = {
+      name: $("#exampleInputEmail1").val().trim(),
+      source: $("#exampleInputPassword1").val().trim(), 
+      // location: ,
+      // type:,
+      
+    }
+    if (user.name == '' || user.source == ''){
 
+      alert("it works");
+      return false;
+    } else {
+      
+      
+
+
+      // function writeUserData() {
+      //   firebase.database().ref('users/' + userId).set({
+      //     username: name,
+      //     email: email,
+      //     profile_picture : imageUrl
+      //   });
+      // }
+      
       database.ref().push({
-        username: username,
-        password: password,
+        user
       })
+    }
 
       $(".form-control").val('');
     })
 
     database.ref().on("child_added", function(snapshot){
-        console.log(snapshot.val().username);
-        console.log(snapshot.val().password);
+        console.log(snapshot.val().user.name);
+        console.log(snapshot.val().user.source);
     })
 
 
